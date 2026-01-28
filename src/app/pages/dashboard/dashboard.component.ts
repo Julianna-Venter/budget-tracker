fed58b490d29f0cd0fe499b40a350d482166e53b
juliannaventer <juliannaventer@gmail.com>
2026-01-28 06:38:22 +0200

    submit

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CreateTransactionsService } from '../../services/create-transactions/create-transactions.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
- 
+ amount: number = 0;
+
+ constructor(private createTransactionsService: CreateTransactionsService) {
+   this.amount = createTransactionsService.calculateAmount([22.34, 67.89, 110.89, 10]);
+ }
 }
--- src/app/services/create-transactions/create-transactions.service.ts
+++ src/app/services/create-transactions/create-transactions.service.ts
@@ -6,4 +6,16 @@ import { Injectable } from '@angular/core';
 export class CreateTransactionsService {
 
   constructor() { }
+
+  public calculateAmount(transactions: number[]): number {
+    let amount = 0;
+    transactions.forEach(transaction => {
+      amount +=
+        (transaction % 10 === 0
+          ? transaction + 10
+          : Math.ceil(transaction / 10) * 10) - transaction;
+    });
+
+    return amount;
+  }
 }
